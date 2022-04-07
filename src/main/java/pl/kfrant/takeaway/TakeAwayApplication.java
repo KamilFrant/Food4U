a3aae95c706b2bc6cc74a537f1fdb9065db4e6a7
Kamil Frant <kamilfrantmu@wp.pl>
2022-04-07 16:17:45 +0200

    Init commit

---
+++ src/main/java/pl/kfrant/takeaway/TakeAwayApplication.java
@@ -0,0 +1,13 @@
+package pl.kfrant.takeaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeAwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakeAwayApplication.class, args);
+    }
+
+}
