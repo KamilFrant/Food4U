bf715af8357d1426951b15d219e4dde46c7479a4
Kamil Frant <kamilfrantmu@wp.pl>
2022-04-08 17:32:19 +0200

    Add an orders button

--- src/main/java/pl/kfrant/takeaway/dish/DishController.java
+++ src/main/java/pl/kfrant/takeaway/dish/DishController.java
@@ -4,19 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller("/")
+@Controller()
 public class DishController {
 
-    private final DishService dishService;
 
-    public DishController(DishService dishService) {
-        this.dishService = dishService;
-    }
-
-    @GetMapping
-    public String home(Model model){
-        model.addAttribute("dishes",dishService.getAllDishes());
-        return "index";
-    }
 
 }
---
+++ src/main/java/pl/kfrant/takeaway/order/ClientOrder.java
@@ -0,0 +1,29 @@
+package pl.kfrant.takeaway.order;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+import pl.kfrant.takeaway.dish.Dish;
+
+@Component
+@SessionScope
+public class ClientOrder {
+
+    private Order order;
+
+    public ClientOrder() {
+        clear();
+    }
+
+    Order getOrder() {
+        return order;
+    }
+
+    void add(Dish dish) {
+        order.getDishes().add(dish);
+    }
+
+    void clear() {
+        order = new Order();
+        order.setStatus(OrderStatus.NEW);
+    }
+}
